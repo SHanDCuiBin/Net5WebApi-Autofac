2e5f60f3234112c5022d106a1d434b4b86189a05
SHanDCuiBin <SDCuiBin@outlook.com>
2021-06-26 15:51:57 +0800

    添加项目文件。

---
+++ Net5WebApi+Autofac/Service/TestServiceB.cs
@@ -0,0 +1,16 @@
+﻿using Net5WebApi_Autofac.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net5WebApi_Autofac.Service
+{
+    public class TestServiceB : ITestServiceB
+    {
+        public void Show()
+        {
+             Console.WriteLine($"This is a {this.GetType().Name} Instance...");
+        }
+    }
+}
